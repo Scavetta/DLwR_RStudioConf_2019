bd71f84904e2e164b7a3eba441453950610bdbaf
Scavetta <rickyjoey@gmail.com>
2018-11-25 17:19:08 +0100

    Abalone added

---
+++ Abalone/Abalone.R
@@ -0,0 +1,14 @@
+myData <- read.csv("Desktop/abalone.data")
+
+results <- myData[9]
+myData <- myData[1:8]
+
+Sex
+Length
+Diameter
+Height
+Whole_weight
+Shucked_weight
+Viscera_weight
+Shell_weight
+Rings
